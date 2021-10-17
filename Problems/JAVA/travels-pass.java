835f5217b4642e1f3422406b0f3ebd4d2a15e765
PrAjJwAl KuMaR <talk2prajjwal@gmail.com>
2021-10-17 14:04:20 +0530

    Travel Pass CodeChef problem

--- Problems/JAVA/travels-pass.java
+++ Problems/JAVA/travels-pass.java
@@ -1,6 +1,22 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+*Chef is going on a road trip and needs to apply for inter-district and inter-state travel e-passes. It takes A
+ minutes to fill each inter-district e-pass application and B
+ minutes for each inter-state e-pass application.
+
+His journey is given to you as a binary string S
+ of length N
+ where 0
+ denotes crossing from one district to another district (which needs an inter-district e-pass), and a 1
+ denotes crossing from one state to another (which needs an inter-state e-pass).
+
+Find the total time Chef has to spend on filling the various forms.*/
+
+
+
+
 public class TravelPass {
 
     public static void main(String[] args) {
